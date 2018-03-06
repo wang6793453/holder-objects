a2edfdfd48c6a101850fcd35bc6f0b0c1ff91b99
zjun <565061763@qq.com>
2018-03-06 09:16:25 +0800

    demo

--- app/src/main/java/com/fanwe/holder_objects/MainActivity.java
+++ app/src/main/java/com/fanwe/holder_objects/MainActivity.java
@@ -5,14 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.fanwe.lib.holder.objects.FObjectsHolder;
-import com.fanwe.lib.holder.objects.FWeakObjectsHolder;
+import com.fanwe.lib.holder.objects.FStrongObjectsHolder;
 import com.fanwe.lib.holder.objects.ForeachCallback;
 
 public class MainActivity extends AppCompatActivity
 {
     public static final String TAG = MainActivity.class.getSimpleName();
 
-    private FObjectsHolder<Integer> mHolder = new FWeakObjectsHolder<>();
+    private FObjectsHolder<Integer> mHolder = new FStrongObjectsHolder<>();
+//    private FObjectsHolder<Integer> mHolder = new FWeakObjectsHolder<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,15 +28,18 @@ public class MainActivity extends AppCompatActivity
 
     private void fillData()
     {
-        mHolder.clear();
+        mHolder.clear(); //清空所有对象
         for (int i = 0; i < 20; i++)
         {
-            mHolder.add(i);
+            mHolder.add(i); //添加对象
         }
     }
 
     private void doForeach(final boolean log)
     {
+        /**
+         * 正序遍历
+         */
         mHolder.foreach(new ForeachCallback<Integer>()
         {
             @Override
@@ -51,6 +55,9 @@ public class MainActivity extends AppCompatActivity
 
     private void doForeachReverse(final boolean log)
     {
+        /**
+         * 倒序遍历
+         */
         mHolder.foreachReverse(new ForeachCallback<Integer>()
         {
             @Override
